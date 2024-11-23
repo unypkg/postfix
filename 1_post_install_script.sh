3c41aaa335523657030fc78e88ef4145b58af419
michacassola <mich@casso.la>
2024-11-23 17:07:15 +0100

    Update 1_post_install_script.sh

--- 1_post_install_script.sh
+++ 1_post_install_script.sh
@@ -11,11 +11,11 @@ cd "$unypkg_root_dir" || exit
 ### Start of script
 
 mkdir -p /var/mail
-groupadd -g 32 postfix &&
-    groupadd -g 33 postdrop &&
-    useradd -c "Postfix Daemon User" -d /var/spool/postfix -g postfix \
-        -s /bin/false -u 32 postfix &&
-    chown -v postfix:postfix /var/mail
+groupadd -g 32 postfix
+groupadd -g 33 postdrop
+useradd -c "Postfix Daemon User" -d /var/spool/postfix -g postfix \
+    -s /bin/false -u 32 postfix
+chown -v postfix:postfix /var/mail
 
 if [ ! -f /etc/uny/postfix/main.cf ]; then
     install -v -dm 755 -o postfix -g postfix /etc/uny/postfix
